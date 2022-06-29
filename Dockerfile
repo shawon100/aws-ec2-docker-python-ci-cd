b6691ca31cebd68ba4786f74993fd546f0bc9100
shawon100 <shawonashadullah@gmail.com>
2022-06-29 12:01:44 +0600

    Added pipelines

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+﻿FROM python:3.6.4-slim
+WORKDIR /app
+COPY /app /app
+RUN pip install -r /app/requirements.txt
+CMD python /app/main.py 
+EXPOSE 5000
---
+++ app/main.py
@@ -0,0 +1,7 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return "Hello Shawon! Python Flask is running from AWS EC2 Docker"
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True, port=5000)
