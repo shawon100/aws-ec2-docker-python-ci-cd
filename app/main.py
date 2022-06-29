f129175df085ab38f7d06f38fd68cd49253c2ada
shawon100 <shawonashadullah@gmail.com>
2022-06-29 14:06:03 +0600

    updated content

--- app/main.py
+++ app/main.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello Shawon! Python Flask is running from AWS EC2 Docker"
+    return "Hello Shawon! Python Flask is Running from AWS EC2 Docker"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=5000)
